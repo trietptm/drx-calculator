b04fb4469b83ae3455adb8fa8e34d20c59adba77
crackinglandia <crackinglandia@f00cd1d4-5c0d-03bc-cec4-745e7fe35079>
2011-11-22 01:47:26 +0000

    * DRx Calculator GUI created by resedit (fucking great tool!)

---
+++ DRx Calculator GUI/resource.h
@@ -0,0 +1,21 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG1                             100
+#define HELP                                    1000
+#define EXIT                                    1020
+#define CALCULATE                               1021
+#define EDIT_CALCULATE                          1022
+#define CHECK_DR0                               1027
+#define CHECK_DR1                               1028
+#define CHECK_DR2                               1029
+#define CHECK_DR3                               1030
+#define TYPE_DR0                                1031
+#define TYPE_DR1                                1032
+#define TYPE_DR2                                1033
+#define TYPE_DR3                                1034
+#define SIZE_DR0                                1035
+#define SIZE_DR1                                1037
+#define SIZE_DR2                                1038
+#define SIZE_DR3                                1039
